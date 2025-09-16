5a3b480388042e6a766ec5dffaadf01ef8729b8a
klu2300031690 <163740085+klu2300031690@users.noreply.github.com>
2025-09-16 12:16:31 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/cart";
+const API_URL = "http://ec2-54-92-211-116.compute-1.amazonaws.com:8081/api/cart";
 
 const API_BASE = "http://localhost:8081/api/cart";
 
